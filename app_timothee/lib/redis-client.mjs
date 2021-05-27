3be893c311530bc78ca7cd8c58793e05e408938b
Nicolas Chambrier <naholyr@gmail.com>
2021-05-27 21:38:50 +0200

    J2: tests (ESM, Timothée)

--- app_timothee/lib/redis-client.mjs
+++ app_timothee/lib/redis-client.mjs
@@ -1,11 +1,11 @@
-import redis from require("ioredis");
+import Redis from "ioredis";
 
 const options = {
   host: "localhost",
   port: 6378,
 };
 
-const client = new Redis.client(process.env.REDIS_URL);
+const client = new Redis(process.env.REDIS_URL);
 
 module.exports = {
   client,
